d4a51a562387c0304b383e767905833433d9c057
zaidtariq833 <72224680+zaidtariq833@users.noreply.github.com>
2024-09-29 19:24:15 +0500

    forgot password UI with responsive (#6)

--- src/app/forgot-password/page.js
+++ src/app/forgot-password/page.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import ForgetPassword from '../components/authComponents/forgetPasswordComp'
+
+export const metadata = {
+  title: "Forgot Password",
+  description: "Forgot Password for Social Media App"
+}
 
 const page = () => {
   return (
     <div>
-      forgot password
+
+      <ForgetPassword />
     </div>
   )
 }
